dacdea4d52c68912e6b13d0939ade9645fb96fc3
junseopsan <junseopsan@gmail.com>
2023-03-29 15:23:44 +0900

    publish

--- src/app/items/[id]/page.tsx
+++ src/app/items/[id]/page.tsx
@@ -5,7 +5,7 @@ const ItemDetail: NextPage = () => {
     <div className="px-4 py-10">
       <div className="mb-8">
         <div className="h-96 bg-slate-300" />
-        <div className="flex cursor-pointer py-3 border-t border-b items-center space-x-3">
+        <div className="flex items-center py-3 space-x-3 border-t border-b cursor-pointer">
           <div className="w-12 h-12 rounded-full bg-slate-300" />
           <div>
             <p className="text-sm font-medium text-gray-700">Steve Jebs</p>
@@ -16,8 +16,8 @@ const ItemDetail: NextPage = () => {
         </div>
         <div className="mt-5">
           <h1 className="text-3xl font-bold text-gray-900">Galaxy S50</h1>
-          <span className="text-3xl block mt-3 text-gray-900">$140</span>
-          <p className=" my-6 text-gray-700">
+          <span className="block mt-3 text-2xl text-gray-900">$140</span>
+          <p className="my-6 text-gray-700 ">
             My money&apos;s in that office, right? If she start giving me some
             bullshit about it ain&apos;t there, and we got to go someplace else
             and get it, I&apos;m gonna shoot you in the head then and there.
@@ -28,12 +28,12 @@ const ItemDetail: NextPage = () => {
             motherfucker to get shot. You understand?
           </p>
           <div className="flex items-center justify-between space-x-2">
-            <button className="flex-1 bg-orange-500 text-white py-3 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 font-medium hover:bg-orange-600 focus:ring-orange-500 ">
+            <button className="flex-1 py-3 font-medium text-white bg-orange-500 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 hover:bg-orange-600 focus:ring-orange-500 ">
               Talk to seller
             </button>
-            <button className="p-3 rounded-md flex items-center justify-center text-gray-400 hover:bg-gray-100 hover:text-gray-500">
+            <button className="flex items-center justify-center p-3 text-gray-400 rounded-md hover:bg-gray-100 hover:text-gray-500">
               <svg
-                className="h-6 w-6 "
+                className="w-6 h-6 "
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -53,11 +53,11 @@ const ItemDetail: NextPage = () => {
       </div>
       <div>
         <h2 className="text-2xl font-bold text-gray-900">Similar items</h2>
-        <div className=" mt-6 grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-2 gap-4 mt-6 ">
           {[1, 2, 3, 4, 5, 6].map((_, i) => (
             <div key={i}>
-              <div className="h-56 w-full mb-4 bg-slate-300" />
-              <h3 className="text-gray-700 -mb-1">Galaxy S60</h3>
+              <div className="w-full h-56 mb-4 bg-slate-300" />
+              <h3 className="-mb-1 text-gray-700">Galaxy S60</h3>
               <span className="text-sm font-medium text-gray-900">$6</span>
             </div>
           ))}
